0226304c698b078f70dd20b23b7aa9d1d570461a
Luccasrc22 <lucasrios22@hotmail.com>
2023-05-04 18:09:14 -0300

    corrigindo

--- treinamentoOO/POO/src/Programa.java
+++ treinamentoOO/POO/src/Programa.java
@@ -3,9 +3,13 @@ import java.util.*;
 public class Programa{
 	private static List<Livro> livros= new ArrayList<>();
 	public static void main(String[] args){
+		Object object = new Object();
+		object.toString();
+
 		Livro livro = new Livro("Dom Casmurro", 1899);
 		livro.addObs("Observação sem data");
 		livro.addObs("outra observação, agora com data", 22, 10, 2000);
+		System.out.println(livro.toString());;
 	}
 	public static void cadastrarLivro(String titulo, String autor){
 		Livro novoLivro = new Livro(titulo, 0);
