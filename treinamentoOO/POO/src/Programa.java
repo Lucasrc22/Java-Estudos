239ff37130951abbb715b02a41a970fdc845a6cf
Lucasrc22 <lucasrios2210@gmail.com>
2023-05-07 20:49:00 -0300

    poliformismo

--- treinamentoOO/POO/src/Programa.java
+++ treinamentoOO/POO/src/Programa.java
@@ -1,24 +1,12 @@
-
 import java.util.*;
+
 public class Programa{
-	private static List<Livro> livros= new ArrayList<>();
 	public static void main(String[] args){
-		Object object = new Object();
-		object.toString();
+		Object minhaPublicacao = new Livro("Dom Casmurro", 1899);
 
-		Livro livro = new Livro("Dom Casmurro", 1899);
-		livro.addObs("Observação sem data");
-		livro.addObs("outra observação, agora com data", 22, 10, 2000);
-		System.out.println(livro.toString());;
-	}
-	public static void cadastrarLivro(String titulo, String autor){
-		Livro novoLivro = new Livro(titulo, 0);
-		novoLivro.addNomeAutor(autor);
-		livros.add(novoLivro);
-	}
+		
 
-	public static void cadastrarLivro(String titulo, int ano){
-		Livro novoLivro = new Livro(titulo, ano);
-		livros.add(novoLivro);
-	}
+		System.out.println(((Livro)minhaPublicacao).gerarReferencia());
+		
+	}	
 }
