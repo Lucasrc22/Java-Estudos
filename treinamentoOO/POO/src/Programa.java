0efe6001e1e79a00802f66bcc1e403abf6b00ef5
Lucasrc22 <lucasrios2210@gmail.com>
2023-05-15 11:44:48 -0300

    add try catch e finally

--- treinamentoOO/POO/src/Programa.java
+++ treinamentoOO/POO/src/Programa.java
@@ -1,7 +1,24 @@
-
+import java.util.*;
 public class Programa{
+	static ArrayList<Livro> livros = new ArrayList<>();
 	public static void main(String[] args){
-		Referenciavel pub = new Livro("Dragon Ball", 1994);
-		System.out.println(pub.gerarReferencia());
-	}	
+		try{
+			Livro livro1 = new Livro("Dragonball", 1994);
+			cadastrarLivro(livro1);
+			System.out.print("Novo tamanho da lista de livros: ");
+			System.out.println(livros.size());
+		} catch(NullPointerException exception){
+			System.out.println("Valor nulo encontrado");
+		} finally{
+			System.out.println("Até logo");
+		}
+	}
+	
+	static void cadastrarLivro(Livro novoLivro){
+		if(novoLivro.getTitulo().equals("")){
+			System.out.println("Livro sem titulo não é perimitido");
+		}else{
+			livros.add(novoLivro);
+		}
+	}
 }
